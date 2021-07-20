ffe7a6b3a57458d752b2c48104a8343c950b4fcb
thomas <thomasrcisneroz@gmail.com>
2021-07-20 12:32:46 -0700

    commit tree 31a03e8f7f1472b850c77b1a368e95829b450c16

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+   return(`This Saturday, I want to ${activity}!`)
+}
+
+
+function mondayWork(destination="go to the office") {
+    return(`This Monday, I will ${destination}.`)
+}
+
+function wrapAdjective(woo ="*") {
+    return function(emphatic="a dedicated programer"){
+    return (`You are ${woo}${emphatic}${woo}!`);
+    }
+}
